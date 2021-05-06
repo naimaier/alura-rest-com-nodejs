68aa8fb46825c71886726eaf6dd9b65a79e57df4
Henrique Naimaier <coubron@yahoo.com.br>
2021-05-06 15:35:24 -0300

    Add body-parser

--- config/customExpress.js
+++ config/customExpress.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const consign = require('consign')
+const bodyParser = require('body-parser')
 
 module.exports = () => {
     const app = express()
+
+    app.use(bodyParser.urlencoded({extended: true}))
+    app.use(bodyParser.json())
+    // Adicionamos parser de formulário html e json nas configurações do servidor
+    // para poder utilizá-los em todas as requisições
     
     consign()
         .include('controllers')
--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,4 +1,9 @@
 module.exports = app => {
-    app.get('/atendimentos', (request, response) => response.send('Servidor rodando!!'))
+    app.get('/atendimentos', (request, response) => response.send('Você está na rota de atendimentos e realizando um GET'))
+    
+    app.post('/atendimentos', (request, response) => {
+        console.log(request.body)
+        response.send('Você está na rota de atendimentos e realizando um POST')
+    })
 }
 // Estamos exportando uma funcao que recebe app como parametro
