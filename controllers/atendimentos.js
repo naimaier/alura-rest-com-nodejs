64a907922272ef894134e04739d947df74d89734
Henrique Naimaier <coubron@yahoo.com.br>
2021-05-10 09:00:57 -0300

    POST now add 'atendimento' to database

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,9 +1,14 @@
+const Atendimento = require('../models/atendimentos')
+
 module.exports = app => {
     app.get('/atendimentos', (request, response) => response.send('Você está na rota de atendimentos e realizando um GET'))
     
     app.post('/atendimentos', (request, response) => {
-        console.log(request.body)
-        response.send('Você está na rota de atendimentos e realizando um POST')
+        const atendimento = request.body
+
+        Atendimento.adiciona(atendimento)
+        
+        response.send('POST atendimento')
     })
 }
 // Estamos exportando uma funcao que recebe app como parametro
