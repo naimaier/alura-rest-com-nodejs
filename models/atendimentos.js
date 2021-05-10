a1e707eb517b78472fe544e52d160135e0c2e19f
Henrique Naimaier <coubron@yahoo.com.br>
2021-05-10 11:23:02 -0300

    Adding validations

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -5,21 +5,46 @@ class Atendimento {
     adiciona(atendimento, response) {
         const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
         const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
-        const atendimentoDatado = {...atendimento, dataCriacao, data}
-        // Criamos um objeto novo para ser evitar qualquer problema em outras partes do codigo
+        const dataEhValida = moment(data).isSameOrAfter(dataCriacao)
+        const clienteEhValido = atendimento.cliente.length >= 5
 
-        const sql = 'INSERT INTO Atendimentos SET ?'
-
-        conexao.query(sql, atendimentoDatado, (erro, resultados) => {
-            if (erro) {
-                response.status(400).json(erro)
-                // 400: Bad request. O Cliente mandou algum dado inválido
-            } else {
-                response.status(201).json(resultados)
-                // O 'resultados' mostra o insertId entre outros
-                // 201 created
+        const validacoes = [
+            {
+                nome: 'data',
+                valido: dataEhValida,
+                mensagem: 'Data deve ser maior ou igual à data atual'
+            },
+            {
+                nome: 'cliente',
+                valido: clienteEhValido,
+                mensagem: 'Cliente deve ter pelo menos cinco caracteres'
             }
-        })
+        ]
+
+        const erros = validacoes.filter(campo => !campo.valido)
+        // retorna o campo quando este for inválido
+        const existemErros = erros.length
+        // Se lengh for 0 a variável equivale a false
+
+        if (existemErros) {
+            response.status(400).json(erros)
+        } else {
+            const atendimentoDatado = {...atendimento, dataCriacao, data}
+            // Criamos um objeto novo para ser evitar qualquer problema em outras partes do codigo
+    
+            const sql = 'INSERT INTO Atendimentos SET ?'
+    
+            conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+                if (erro) {
+                    response.status(400).json(erro)
+                    // 400: Bad request. O Cliente mandou algum dado inválido
+                } else {
+                    response.status(201).json(resultados)
+                    // O 'resultados' mostra o insertId entre outros
+                    // 201 created
+                }
+            })
+        }
     }
 }
 
